a028aa8de01f68a17b0b27be272537a752bdc56f
vmariano <vmariano@users.noreply.github.com>
2019-04-08 15:53:36 -0300

    Transform in arrow functions, to avoid bindings.

--- src/components/quantity-selector/QuantitySelector.jsx
+++ src/components/quantity-selector/QuantitySelector.jsx
@@ -6,24 +6,22 @@ export class QuantitySelector extends React.Component {
     constructor(props) {
         super(props);
         this.state = {currentAmount: this.props.amount};
-        this.add = this.increment.bind(this);
-        this.remove = this.decrement.bind(this);
     }
 
-    increment() {
-        this.setState({currentAmount: this.state.currentAmount + 1 });
-    }
+    increment = () => {
+        this.setState({currentAmount: this.state.currentAmount + 1});
+    };
 
-    decrement() {
-        this.setState({currentAmount: this.state.currentAmount - 1 });
-    }
+    decrement = () => {
+        this.setState({currentAmount: this.state.currentAmount - 1});
+    };
 
     render() {
         return (
             <div className="quantity-selector">
-                <button onClick={this.add}>+</button>
+                <button onClick={this.increment}>+</button>
                 <label>{this.state.currentAmount}</label>
-                <button onClick={this.remove}>-</button>
+                <button onClick={this.decrement}>-</button>
             </div>
         );
     }
