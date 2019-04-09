95d5f253e82eed5e512f92d9310018b2e7c08a6e
Don Vicente <m.vicent@gmail.com>
2019-04-09 11:01:01 -0300

    Add quantity component (#1)
    * Add quantity component.
    * Transform in arrow functions, to avoid bindings.
    * Wrap set in functions.

---
+++ src/components/quantity-selector/QuantitySelector.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './QuantitySelector.css'
+
+export class QuantitySelector extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {currentAmount: this.props.amount};
+    }
+
+    increment = () => {
+        this.setState((state) => {
+            return {currentAmount: state.currentAmount + 1}
+        });
+    };
+
+    decrement = () => {
+        this.setState((state) => {
+            return {currentAmount: state.currentAmount - 1}
+        });
+    };
+
+    render() {
+        return (
+            <div className="quantity-selector">
+                <button onClick={this.increment}>+</button>
+                <label>{this.state.currentAmount}</label>
+                <button onClick={this.decrement}>-</button>
+            </div>
+        );
+    }
+
+}
