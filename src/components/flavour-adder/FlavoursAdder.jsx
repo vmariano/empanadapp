e0361ddc3a6554020783bb4ac2cc7678c434f4c8
vmariano <vmariano@users.noreply.github.com>
2019-04-09 11:55:23 -0300

    Add flavour adder.

---
+++ src/components/flavour-adder/FlavoursAdder.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {QuantitySelector} from "../quantity-selector/QuantitySelector";
+import "./FlavoursAdder.css"
+
+export class FlavoursAdder extends Component {
+
+    constructor() {
+        super();
+        this.emoji = '🥩';
+    }
+
+    render() {
+        return (
+            <p className="flavour-adder">
+                <i className="em em-information_source" />
+                <label> {this.props.flavour} </label>
+                <QuantitySelector amount={this.props.amount}/>
+            </p>
+        )
+    }
+}
+
+
